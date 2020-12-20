ce4c60b355167baf43212ae87410fff7241d6738
Ilqson <75951046+Ilqson@users.noreply.github.com>
2020-12-20 10:39:30 -0400

    API Projeto

---
+++ REPOSITÓRIO/Projeto API/petshop.api/Controllers/AnimalController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using petshop.api.model;
+using petshop.api.services;
+
+namespace petshop.api.Controllers
+{
+    [Route("api[controller]")]
+    [ApiController]
+    public class AnimalController : Controller
+    {
+        public readonly MongoDbService _mongoDbService;
+
+        public AnimalController(){
+            _mongoDbService = new MongoDbService("AnimalDatabase", "Animal", "mongodb://localhost:27017");
+        }
+
+        [HttpPost]
+        public async Task Cadastrar([FromBody] Animal animal){
+            await _mongoDbService.Insere(animal);
+        }
+    }
+
+}
